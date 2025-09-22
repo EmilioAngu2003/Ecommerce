2838392a2ba786c0a240b55413bcbe10ff4f805b
Emilio <emilio.angu.2003@gmail.com>
2025-09-22 14:29:13 +0200

    feat: Add JWT Bearer authentication to the application

--- Ecommerce.Api/Program.cs
+++ Ecommerce.Api/Program.cs
@@ -1,5 +1,8 @@
 using ECommerce.Application;
 using ECommerce.Infrastructure;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Ecommerce.Api;
 
@@ -14,6 +17,26 @@ public class Program
 
         builder.Services.AddControllers();
 
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                };
+            });
+
+        builder.Services.AddAuthorization();
 
         var app = builder.Build();
 
@@ -21,6 +44,7 @@ public class Program
 
         app.UseHttpsRedirection();
 
+        app.UseAuthentication();
         app.UseAuthorization();
 
 
